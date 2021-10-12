dc5429de51536c0969779a048b12591b51c47c23
SucramYelrut <86125719+SucramYelrut@users.noreply.github.com>
2021-10-12 08:39:05 -0500

    Create Basic_TeleOp.java

---
+++ Practice/Basic_TeleOp.java
@@ -0,0 +1,13 @@
+/*
+ * Define a TeleOp that contains:
+ * 
+ * Move a DCMotor named "Leg" at 0.5 speed for 2 seconds
+ * Set a CRServo named "Ankle"'s power to 1 for 1 second
+ * Set a Servo named "Big Toe"'s position
+ * Set a Servo named "Little Toe"'s position
+ */
+
+(file="Basic_TeleOp", group="TeleOp")
+public class Basic_TeleOp {
+  
+}
