bb6e16e3a7d30627a6e503ad56c74b1258d57734
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-15 14:14:36 -0500

    Update and rename Lessons/Basic_Hardware.java to Lessons/Basic/Basic_Hardware.java

--- Lessons/Basic/Basic_Hardware.java
+++ Lessons/Basic/Basic_Hardware.java
@@ -13,17 +13,7 @@ public class Hardware extends LinearOpMode {
   //As you can see we extend from the audience class and reference it with the signature legoArm.
   //Here is another example.
   DCMotor humanAnkle;
-  
-  //Becuase this in Java we can make an array of Motors
-  DCMotor[] Motors;
-  
-  //We can create a method that we call at runtime called initialize that initializes all of the refrences and variables.
-  //Notice how this void is protected. We use protected to obtain parent methods inside child classes without the need to import the methods
-  //We do this because we are going to make out Runtime_Autonomous and out RunTime_TeleOp extend from this.
-  protected static void init() {
-    //We can obtain methods from other classes to organize and shorten code
-    
-    //However variables we must extends from LinearOpMode cannot be accessed inside of the static classes so we must do so in this class
-    
-  }
+	
+	// We can do the same for Servos (are small mechines that can rotate to setPositions however have a set amount of power
+  Servos humanAnkle;
 }
