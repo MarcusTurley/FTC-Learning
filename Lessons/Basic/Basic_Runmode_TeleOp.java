0004556e78269d46a727c10f862141f2dd704393
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-18 08:30:15 -0500

    Update Basic_Runmode_TeleOp.java

--- Lessons/Basic/Basic_Runmode_TeleOp.java
+++ Lessons/Basic/Basic_Runmode_TeleOp.java
@@ -3,15 +3,17 @@ package org.firstinspires.ftc.teamcode.Lessons.Basic.Basic_Runmode_TeleOp.java;
 //This is the import for Basic_Hardware
 import org.firstinspires.ftc.teamcode.Lessons.Basic.Basic_Hardware.java;
 
+@TeleOp(name= "RunMode: Basic_TeleOpTeleOp", groupd = "TeleOp")
 //We extend Basic_Hardware to be able to reference it's methods
-public class Basic_RunMode_TeleOp extends Basic_Hardware {  
+public class Basic_TeleOp extends Basic_Hardware {
+	
 	//This overrides the runOpMode method located inside of LinearOpMode
-	//This method exicutes when the program first run button is pressed
+	//This method exicutes when the program is intialized
   @Override
 	public void runOpMode() {
 		TeleOp();
 
-		//This will wait for the second run button to be pressed and continue compilation
+		//This will wait for the run button to be pressed and continue compilation
 		waitForStart();
     
     initialize(); //Refrences initialize from Basic_Hardware and calls it
