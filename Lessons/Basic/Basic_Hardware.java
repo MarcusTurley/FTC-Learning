d1b22dcab7828e6a70e7ecb538d8ad050aed6f35
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-18 09:30:48 -0500

    Update Basic_Hardware.java

--- Lessons/Basic/Basic_Hardware.java
+++ Lessons/Basic/Basic_Hardware.java
@@ -10,10 +10,10 @@ public class Hardware extends LinearOpMode {
   //When you want to add something to your lego collection you must add something to your lego box.
   
   //Here is an example of how to define a motor.
-  DCMotor legRight; //@Description	This creates a new DCMotor object called legRight
+  DCMotor legRight;	//@Description	This creates a new DCMotor object called legRight
 	
   //Here is another example.
-  DCMotor legLeft; //@Description	This creates a new DCMotor object called legLeft
+  DCMotor legLeft;	//@Description	This creates a new DCMotor object called legLeft
 	
 	// We can do the same for Servos (are small mechines that can rotate to setPositions however have a set amount of power
 	Servos ankleRight;	//@Description	This creates a new Servo object called ankleRight
@@ -23,11 +23,11 @@ public class Hardware extends LinearOpMode {
 	protected void initialize() {
 		
 		//This is where we refrence the names from the phone configuration so that the Control Hub can assign the objects to it.
-		legRight = hardwareMap.get(DcMotor.class, "RightLeg"); //@Description This assigns the ankleRight to rightAnkle
-		legLeft = hardwareMap.get(DcMotor.class, "LeftLeg"); //@Description This assigns the ankleRight to leftAnkle
+		legRight = hardwareMap.get(DcMotor.class, "RightLeg");	//@Description This assigns the ankleRight to rightAnkle
+		legLeft = hardwareMap.get(DcMotor.class, "LeftLeg");	//@Description This assigns the ankleRight to leftAnkle
 		
-		ankleRight = hardwareMap.get(Servo.class, "RightAnkle"); //@Description This assigns the ankleRight to rightAnkle
-		ankleLeft = hardwareMap.get(Servo.class, "LeftAnkle"); //@Description This assigns the ankleRight to rightAnkle
+		ankleRight = hardwareMap.get(Servo.class, "RightAnkle");	//@Description This assigns the ankleRight to rightAnkle
+		ankleLeft = hardwareMap.get(Servo.class, "LeftAnkle");		//@Description This assigns the ankleRight to rightAnkle
 	}
 	
 	//Note we do not call runOpMode inside of this class, instead we call.
