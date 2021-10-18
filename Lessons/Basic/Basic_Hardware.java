ce83e67307715046e94f65ea200b5a8800a0313a
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-18 09:45:25 -0500

    Update Basic_Hardware.java

--- Lessons/Basic/Basic_Hardware.java
+++ Lessons/Basic/Basic_Hardware.java
@@ -1,19 +1,18 @@
+// @Note This lesson will explain what a Hardware is, how it functions, and how to structure it.
 // @Note @Description is not essential to understanding what is going on but is useful it you lose track
 
 //We can create a class called Hardware and extend LinearOpMode.
 //LinearOpMode contains a number of methods and variables that help structure and define the program for runtime.
 public class Hardware extends LinearOpMode {
-  //This lesson will explain what a Hardware is, how it functions, and how to structure it.
-  
-  //A 'Hardware' is a structure that holds all of the .
-  //You can imagine a hardware as a box of legos.
-  //When you want to add something to your lego collection you must add something to your lego box.
-  
-  //Here is an example of how to define a motor.
-  DCMotor legRight;	//@Description	This creates a new DCMotor object called legRight
+	//A 'Hardware' is a structure that holds all of the .
+	//You can imagine a hardware as a box of legos.
+	//When you want to add something to your lego collection you must add something to your lego box.
 	
-  //Here is another example.
-  DCMotor legLeft;	//@Description	This creates a new DCMotor object called legLeft
+	//Here is an example of how to define a motor.
+	DCMotor legRight;	//@Description	This creates a new DCMotor object called legRight
+	
+	//Here is another example.
+	DCMotor legLeft;	//@Description	This creates a new DCMotor object called legLeft
 	
 	// We can do the same for Servos (are small mechines that can rotate to setPositions however have a set amount of power
 	Servos ankleRight;	//@Description	This creates a new Servo object called ankleRight
@@ -21,14 +20,13 @@ public class Hardware extends LinearOpMode {
 	
 	//This will be called inside of Basic_TeleOp inside the runOpMode method
 	protected void initialize() {
-		
 		//This is where we refrence the names from the phone configuration so that the Control Hub can assign the objects to it.
 		legRight = hardwareMap.get(DcMotor.class, "RightLeg");	//@Description This assigns the ankleRight to rightAnkle
 		legLeft = hardwareMap.get(DcMotor.class, "LeftLeg");	//@Description This assigns the ankleRight to leftAnkle
 		
 		ankleRight = hardwareMap.get(Servo.class, "RightAnkle");	//@Description This assigns the ankleRight to rightAnkle
-		ankleLeft = hardwareMap.get(Servo.class, "LeftAnkle");		//@Description This assigns the ankleRight to rightAnkle
+		ankleLeft = hardwareMap.get(Servo.class, "LeftAnkle");	//@Description This assigns the ankleRight to rightAnkle
 	}
 	
-	//Note we do not call runOpMode inside of this class, instead we call.
+	//Note we do not call runOpMode inside of this class, instead we call it inside of Basic_TeleOp and Basic_Autonomous. This was done si that 
 }
