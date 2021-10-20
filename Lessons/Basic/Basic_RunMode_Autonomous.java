06e9306f95fe94d98af326206a5e3f7b1254c9ca
MarcusTurley <86125719+MarcusTurley@users.noreply.github.com>
2021-10-20 14:15:32 -0500

    Create Basic_RunMode_Autonomous.java

---
+++ Lessons/Basic/Basic_RunMode_Autonomous.java
@@ -0,0 +1,42 @@
+//@Note It is recemmended that you read Basic_Hardware before viewing this class
+
+package org.firstinspires.ftc.teamcode.Lessons.Basic.Basic_RunMode_Autonomous.java;
+
+//This is the import for Basic_Hardware
+import org.firstinspires.ftc.teamcode.Lessons.Basic.Basic_Hardware.java;
+
+@Autonomous(name= "RunMode: Basic_RunMode_Autonomous", group = "Autonomous")
+//We extend Basic_Hardware to be able to reference it's methods
+public class Basic_RunMode_Autonomous extends Basic_Hardware {
+	
+	//This overrides the runOpMode method from the LinearOpMode class and when the program is exicuted
+	@Override
+	public void runOpMode() {
+		//We call the initialize from the Basic_Hardware class
+		Basic_Hardware.initialize();
+
+		//This will wait for the run button to be pressed and continue compilation
+		waitForStart();	//@Description This is called from LinearOpMode
+    
+    MoveArms(1, 1, 0.5, 0, 1000);
+    MoveClamps(1, 1);
+    MoveClaws(1, 1);
+	}
+	
+	private void MoveArms(double leftForward, double leftHoricontal,double rightForward,, double righHorizontal, double waitTime) {
+		ArmLeft.setPower(leftForward + leftHoricontal);
+		ArmLeft.setPower(rightForward + righHorizontal);
+    sleep(waitTime);
+	}
+	
+	private void MoveClamps(boolean rightPosition, boolean leftPosition) {
+			ClampRight.setPosition(rightPosition);
+			ClampLeft.setPosition(leftPosition);
+	}
+	
+	private void MoveClaws(double leftPower, double rightPower) {
+		ClawRight.setPower(leftPower);
+		ClawLeft.setPower(rightPower);
+    sleep(500);
+	}
+}
