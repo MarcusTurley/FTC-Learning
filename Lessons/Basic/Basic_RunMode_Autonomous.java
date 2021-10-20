864681f8aa8450a54a4f99afd3397a79aa7b61e7
Protosurge8626 <74573955+FTCProtosurge8626@users.noreply.github.com>
2021-10-20 15:08:57 -0500

    Update Basic_RunMode_Autonomous.java

--- Lessons/Basic/Basic_RunMode_Autonomous.java
+++ Lessons/Basic/Basic_RunMode_Autonomous.java
@@ -1,9 +1,10 @@
 //@Note It is recemmended that you read Basic_Hardware before viewing this class
-
-package org.firstinspires.ftc.teamcode.Lessons.Basic.Basic_RunMode_Autonomous.java;
+//This contains the folder the porogram is found in within in the Android Studio project and must be inside program; typically it is above all the other code.
+package org.firstinspires.ftc.teamcode.Lessons.Basic.Basic_Runmode_TeleOp.java;
 
 //This is the import for Basic_Hardware
-import org.firstinspires.ftc.teamcode.Lessons.Basic.Basic_Hardware.java;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous(name= "RunMode: Basic_RunMode_Autonomous", group = "Autonomous")
 //We extend Basic_Hardware to be able to reference it's methods
@@ -13,7 +14,7 @@ public class Basic_RunMode_Autonomous extends Basic_Hardware {
 	@Override
 	public void runOpMode() {
 		//We call the initialize from the Basic_Hardware class
-		Basic_Hardware.initialize();
+		initialize();
 
 		//This will wait for the run button to be pressed and continue compilation
 		waitForStart();	//@Description This is called from LinearOpMode
@@ -26,14 +27,14 @@ public class Basic_RunMode_Autonomous extends Basic_Hardware {
 	
 	
 	//Moves the arms based on the given inputs
-	private void MoveArms(double leftForward, double leftHoricontal,double rightForward,, double righHorizontal, double waitTime) {
+	private void MoveArms(double leftForward, double leftHoricontal,double rightForward, double righHorizontal, int waitTime) {
 		ArmLeft.setPower(leftForward + leftHoricontal);
 		ArmLeft.setPower(rightForward + righHorizontal);
     		sleep(waitTime);
 	}
 	
 	//Moves the clamps based on the given inputs
-	private void MoveClamps(boolean rightPosition, boolean leftPosition) {
+	private void MoveClamps(double rightPosition, double leftPosition) {
 			ClampRight.setPosition(rightPosition);
 			ClampLeft.setPosition(leftPosition);
 	}
